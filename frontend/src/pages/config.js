10adcb880041633128e06e6dee386668ed4a444c
Jane Otaniel <otanieljane@gmail.com>
2026-05-19 12:49:04 +0200

    Connect frontend to render

--- frontend/src/pages/config.js
+++ frontend/src/pages/config.js
@@ -1,8 +1,4 @@
-// src/config.js
-
-// This automatically detects if you are developing on your laptop or if the site is live on the internet
-const API_BASE_URL = window.location.hostname === "localhost"
-  ? "http://localhost:5000"
-  : "https://skills-bloom-api.onrender.com"; // We will update this specific link later
+// src/pages/config.js (or config.jsx)
+const API_BASE_URL = "https://skillsbloom-api.onrender.com"; 
 
 export default API_BASE_URL;
