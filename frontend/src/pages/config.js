893706eb829068704c496cd4e0f408e2c0af149d
Jane Otaniel <otanieljane@gmail.com>
2026-05-21 15:07:02 +0200

    changes git commit -m changes

--- frontend/src/pages/config.js
+++ frontend/src/pages/config.js
@@ -1,4 +1,4 @@
 // src/pages/config.js
-const API_BASE_URL = "http://localhost:5000"; 
+const API_BASE_URL = "https://skillsBloom-api.onrender.com"; // Your live Render web service URL
 
 export default API_BASE_URL;
