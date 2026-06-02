b1b83d238d9e1ddce929e51a8096cc2f20f16cba
Yami <yamichinombo@gmail.com>
2026-06-02 13:18:37 +0200

    Revert "dynamic mentors and time management"
    This reverts commit 8cb8f4a910645de6adfec386fb638c4cf8d6ace6.

--- schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS mentor_availability (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    mentor_id INT NOT NULL,
-    day VARCHAR(20),
-    start_time TIME,
-    end_time TIME,
-    FOREIGN KEY (mentor_id) REFERENCES users(id)
-);
